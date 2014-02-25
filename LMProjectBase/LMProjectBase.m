f54c97da61aa3697efd7676a8fd7d6552c8debf4
Lukasz Dworakowski <lukaszdworakowski@MacBook-Pro-Lukasz.local>
2014-02-25 10:30:52 +0100

    Initial Commit

---
+++ LMProjectBase/LMProjectBase.m
@@ -0,0 +1,13 @@
+//
+//  LMProjectBase.m
+//  LMProjectBase
+//
+//  Created by Lukasz Dworakowski on 25.02.2014.
+//  Copyright (c) 2014 Lukasz Dworakowski. All rights reserved.
+//
+
+#import "LMProjectBase.h"
+
+@implementation LMProjectBase
+
+@end
